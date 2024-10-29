f0a8bc2a3532da95680dec342d01eed7a9708585
RangaJaladurgam <rangajaladurgam7258@gmail.com>
2024-10-29 12:32:42 +0530

    Creating AdminMapper and Adding it as a Dependency in adminService

--- src/main/java/com/pharmassist/service/AdminService.java
+++ src/main/java/com/pharmassist/service/AdminService.java
@@ -1,12 +1,15 @@
 package com.pharmassist.service;
 
+import com.pharmassist.mapper.AdminMapper;
 import com.pharmassist.repository.AdminRepository;
 
 public class AdminService {
 	
 	private AdminRepository adminRepository;
+	private AdminMapper adminMapper;
 
-	public AdminService(AdminRepository adminRepository) {
+	public AdminService(AdminRepository adminRepository,AdminMapper adminMapper) {
 		this.adminRepository = adminRepository;
+		this.adminMapper = adminMapper;
 	}
 }
