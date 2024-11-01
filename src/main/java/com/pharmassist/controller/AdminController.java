24d92c63e2bf8e053f4b9ec8a97390ea9ecd4ce2
RangaJaladurgam <rangajaladurgam7258@gmail.com>
2024-11-01 12:34:39 +0530

    Working on Admin Documentation

--- src/main/java/com/pharmassist/controller/AdminController.java
+++ src/main/java/com/pharmassist/controller/AdminController.java
@@ -10,16 +10,24 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.pharmassist.requestdto.AdminRequest;
 import com.pharmassist.responsedto.AdminResponse;
 import com.pharmassist.service.AdminService;
 import com.pharmassist.util.AppResponseBuilder;
+import com.pharmassist.util.ErrorStructure;
 import com.pharmassist.util.ResponseStructure;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
-@Controller
+@RestController
+@Tag(name="Admin Controller",description = "The controller provides end-points to operate on Admin Entity")
 public class AdminController {
 
 	private final AdminService adminService;
@@ -30,24 +38,82 @@ public class AdminController {
 		this.response = response;
 	}
 	
+	
+	
+	@Operation(description = "The End-point can be used to save the Admin",
+			responses = {
+					@ApiResponse(responseCode = "201",description = "Admin Created",
+							content = {
+									@Content(schema = @Schema(implementation = AdminResponse.class))
+										}
+							
+								),
+					@ApiResponse(responseCode = "400",description = "Bad AdminRequest, Invalid Input",
+							content = {
+									@Content(schema = @Schema(implementation = ErrorStructure.class))
+							}
+							)
+						}
+			)
 	@PostMapping("/admins")
 	public ResponseEntity<ResponseStructure<AdminResponse>> saveAdmin(@RequestBody @Valid AdminRequest adminRequest){
 		AdminResponse adminResponse = adminService.saveUser(adminRequest);
 		return response.success(HttpStatus.CREATED, "Admin Created", adminResponse);
 	}
 	
+	
+	@Operation(description = "The End-point can be used to find the Admin",
+			responses = {
+					@ApiResponse(responseCode = "302",description = "Admin Found",
+							content = {
+									@Content(schema = @Schema(implementation = AdminResponse.class))
+							}),
+					@ApiResponse(responseCode = "404",description = "Admin Not Found",
+							content = {
+									@Content(schema = @Schema(implementation = ErrorStructure.class))
+							})
+			}
+			)
 	@GetMapping("/admins/{adminId}")
 	public ResponseEntity<ResponseStructure<AdminResponse>> findAdmin(@PathVariable String adminId){
 		AdminResponse adminResponse = adminService.findAdmin(adminId);
 		return response.success(HttpStatus.FOUND,"Admin found by Id", adminResponse);
 	}
 	
+	
+	@Operation(description = "The End-point can be used to update the Admin",
+			responses = {
+					@ApiResponse(responseCode = "200",description = "Admin Updated",
+							content = {
+									@Content(schema = @Schema(implementation = AdminResponse.class))
+							}),
+					@ApiResponse(responseCode = "400",description = "Bad AdminRequest, Invalid Input",
+							content = {
+									@Content(schema = @Schema(implementation = ErrorStructure.class))
+					}),
+					@ApiResponse(responseCode = "404",description = "Admin Not Found",
+							content = {
+									@Content(schema = @Schema(implementation = ErrorStructure.class))
+							})
+			})
 	@PutMapping("/admins/{adminId}")
 	public ResponseEntity<ResponseStructure<AdminResponse>> updateAdmin(@PathVariable String adminId,@RequestBody @Valid AdminRequest adminRequest){
 		AdminResponse adminResponse = adminService.updateAdmin(adminId,adminRequest);
 		return response.success(HttpStatus.OK, "Admin Updated", adminResponse);
 	}
 	
+	
+	@Operation(description = "The End-point can be used to List all Admins",
+			responses = {
+					@ApiResponse(responseCode = "302",description = "Admins Found",
+							content = {
+									@Content(schema = @Schema(implementation = AdminResponse.class))
+							}),
+					@ApiResponse(responseCode = "404",description = "No Admins Found",
+							content = {
+									@Content(schema = @Schema(implementation = ErrorStructure.class))
+							})
+			})
 	@GetMapping("/admins")
 	public ResponseEntity<ResponseStructure<List<AdminResponse>>> findAllAdmins(){
 		List<AdminResponse> adminResponses = adminService.findAllAdmins();
