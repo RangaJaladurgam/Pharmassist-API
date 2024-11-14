fedf376a7df6048e05ab3c60b6e61a18dd164314
RangaJaladurgam <rangajaladurgam7258@gmail.com>
2024-11-14 12:54:29 +0530

    Creating transaction Entity

--- src/main/java/com/pharmassist/entity/Pharmacy.java
+++ src/main/java/com/pharmassist/entity/Pharmacy.java
@@ -28,6 +28,22 @@ public class Pharmacy {
 	@OneToMany(mappedBy = "pharmacy")
 	private List<Medicine> medicines;
 	
+	@OneToMany(mappedBy = "pharmacy")
+	private List<Transaction> transactions;
+	
+	
+	public List<Medicine> getMedicines() {
+		return medicines;
+	}
+	public void setMedicines(List<Medicine> medicines) {
+		this.medicines = medicines;
+	}
+	public List<Transaction> getTransactions() {
+		return transactions;
+	}
+	public void setTransactions(List<Transaction> transactions) {
+		this.transactions = transactions;
+	}
 	public Admin getAdmin() {
 		return admin;
 	}
