e85995eefa62578933803e6f766a330250d7587a
RangaJaladurgam <rangajaladurgam7258@gmail.com>
2024-12-01 11:19:24 +0530

    Creating Html Templete for pdf and PdfService, Implemented end-point for generating bill

--- src/main/java/com/pharmassist/controller/BillController.java
+++ src/main/java/com/pharmassist/controller/BillController.java
@@ -1,5 +1,11 @@
 package com.pharmassist.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,15 +21,20 @@ import com.pharmassist.entity.Bill;
 import com.pharmassist.enums.PaymentMode;
 import com.pharmassist.responsedto.BillResponse;
 import com.pharmassist.service.BillService;
+import com.pharmassist.service.PdfService;
 import com.pharmassist.util.AppResponseBuilder;
 import com.pharmassist.util.ResponseStructure;
 import com.pharmassist.util.SimpleResponseStructure;
 import com.pharmassist.util.UpdateQuantityRequest;
 
+import jakarta.servlet.http.HttpServletResponse;
+
 
 @RestController
 public class BillController {
 	
+	@Autowired
+    private PdfService pdfService;
 	private final BillService billService;
 	private final AppResponseBuilder response;
 	
@@ -65,11 +76,52 @@ public class BillController {
 	}
 	
 	@GetMapping("/bills/find/{billId}")
-	public ResponseEntity<ResponseStructure<BillResponse>> getBill(@PathVariable String billId){
-		BillResponse bill = billService.getBill(billId);
+	public ResponseEntity<ResponseStructure<Bill>> getBill(@PathVariable String billId){
+		Bill bill = billService.getBill(billId);
 		return response.success(HttpStatus.FOUND, "bill found", bill);
 	}
-	
+
+
+    @GetMapping("/bills/{billId}/generate")
+    public void generateZomatoBillPdf(@PathVariable String billId,HttpServletResponse response) throws Exception {
+    	Bill bill = billService.getBill(billId);
+    	
+        // Sample data for the Zomato bill
+        Map<String, Object> data = new HashMap<>();
+        data.put("orderId", bill.getBillId());
+        data.put("customerName", bill.getPatient().getName());
+        data.put("customerMobile", bill.getPatient().getPhoneNumber());
+        data.put("pharmacyname", bill.getPatient().getPharmacy().getName());
+        
+        List<Map<String, Object>> items = bill.getBag().getBillItems().stream()
+                .map(item -> {
+                    Map<String, Object> map = new HashMap<>();
+                    map.put("name", item.getName());
+                    map.put("quantity", item.getQuantity());
+                    map.put("priceperitem", item.getPricePerItem());
+                    map.put("price", item.getTotalPrice());
+                    return map;
+                })
+                .collect(Collectors.toList());
+
+
+        data.put("items", items);
+        
+        
+        data.put("totalAmount", bill.getTotalAmount());
+        data.put("totalPayAmount", bill.getTotalPayableAmount());
+        data.put("paymentMode",bill.getPaymentMode());
+        data.put("dateTime", bill.getDateTime());
+
+        // Generate PDF
+        byte[] pdfBytes = pdfService.generatePdf("pharmacy-bill", data);
+
+        
+        // Set response headers
+        response.setContentType("application/pdf");
+        response.setHeader("Content-Disposition", "attachment; filename=pharmacy-bill.pdf");
+        response.getOutputStream().write(pdfBytes);
+    }
 	
 	
 	
